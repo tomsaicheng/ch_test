93be56d5cefdbc4efba3700f5cecf3f6aeffae91
chenghui <tomshacs@vip.qq.com>
2018-09-07 14:47:39 +0800

    0907

--- redis.py
+++ redis.py
@@ -4,13 +4,14 @@
 # r = redis.Redis(host='10.16.2.49', port=6379, db=0)
 # print r.get('product_getProductVoById')
 
-import redis
+import redis, datetime
 
 
 r = redis(host='10.16.2.49', port=6379, db=0)
 print(r.get('product_getProductVoById'))
 
 print 1
+print datetim.datetime.now()
 
 
 
